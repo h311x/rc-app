89b7f35bfc4089961979c67a173e6801efc7eabf
h311x <33266001+h311x@users.noreply.github.com>
2024-05-13 07:05:29 +0300

    init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,157 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use crossbeam_channel::{unbounded, Sender};
+use macos_accessibility_client::accessibility;
+use rdev::{display_size, exit_grab, grab, set_is_main_thread, Event, EventType};
+use std::{
+    collections::HashMap,
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+use tauri::{AppHandle, Manager};
+
+#[tauri::command]
+fn start_grab(app_handle: AppHandle, sender: tauri::State<SenderState>) {
+    let sender = sender.0.clone();
+
+    thread::spawn(move || {
+        accessibility::application_is_trusted_with_prompt();
+
+        let h = app_handle.clone();
+        set_is_main_thread(false);
+        grab(move |event| {
+            let _ = h.emit_all("event", &event);
+            sender.try_send(event.clone()).unwrap();
+            match event.event_type {
+                // EventType::KeyPress(_) => None,
+                _ => Some(event),
+            }
+        })
+        .expect("could not listen events");
+    });
+}
+
+#[tauri::command]
+fn stop_grab() {
+    exit_grab().expect("Could not stop grab");
+}
+
+#[tauri::command]
+fn test() {
+    let stream = Arc::new(Mutex::new(TcpStream::connect("localhost:1337").unwrap()));
+    stream.lock().unwrap().write_all(b"1").unwrap();
+    let s = Arc::clone(&stream);
+    /*TODO:
+     * - get current screen size
+     * - send it to the server
+     * - figure out how to calculate mouse movement
+     * */
+
+    let mut buffer = [0; 128];
+    thread::spawn(move || loop {
+        match stream.lock().unwrap().read(&mut buffer) {
+            Ok(n) => {
+                if n > 0 {
+                    let message: String = String::from_utf8_lossy(&buffer[..n]).into();
+                    println!("Message from server: {message}")
+                } else {
+                    println!("break");
+                    break;
+                }
+            }
+
+            Err(e) => {
+                println!("ErrorKind: {e}");
+                break;
+            }
+        }
+    });
+
+    // thread::spawn(move || {
+    //     thread::sleep(Duration::from_secs(2));
+    //     s.lock().unwrap().shutdown(std::net::Shutdown::Both);
+    // });
+}
+
+struct SenderState(Sender<Event>);
+
+fn main() {
+    let (send, recv) = unbounded();
+    let clients: Arc<Mutex<HashMap<std::net::SocketAddr, Arc<TcpStream>>>> =
+        Arc::new(Mutex::new(HashMap::new()));
+
+    let c = Arc::clone(&clients);
+
+    thread::spawn(move || {
+        for m in recv {
+            let json = serde_json::to_string(&m).expect("Could not stringify json");
+            for s in clients.lock().unwrap().values() {
+                s.as_ref()
+                    .write_all(json.as_bytes())
+                    .expect("Could not write to stream");
+            }
+        }
+        println!("Connection Closed");
+    });
+
+    thread::spawn(move || {
+        let listener = TcpListener::bind("localhost:1337").expect("could not start tcp listener");
+        println!("Server Started...");
+
+        let mut buffer = [0; 128];
+
+        for stream in listener.incoming() {
+            let c = Arc::clone(&c);
+
+            let mut stream = stream.expect("some stream error??");
+
+            c.lock().unwrap().insert(
+                stream.peer_addr().unwrap(),
+                Arc::new(stream.try_clone().unwrap()),
+            );
+
+            println!("{c:?}");
+
+            println!("New Connection");
+
+            thread::spawn(move || {
+                loop {
+                    match stream.read(&mut buffer) {
+                        Ok(n) => {
+                            if n > 0 {
+                                let message: String = String::from_utf8_lossy(&buffer[..n]).into();
+                                println!("Message from client: {message}")
+                            } else {
+                                break;
+                            }
+                        }
+
+                        Err(e) => {
+                            println!("ErrorKind: {e}");
+                            break;
+                        }
+                    }
+                }
+                let mut c = c.lock().unwrap();
+                c.remove(&stream.peer_addr().unwrap());
+
+                if c.len() == 0 {
+                    exit_grab().expect("Could not stop grab");
+                }
+
+                println!("Connection Closed")
+            });
+        }
+
+        println!("Server Stopped...");
+    });
+
+    tauri::Builder::default()
+        .manage(SenderState(send))
+        .invoke_handler(tauri::generate_handler![test, start_grab, stop_grab])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+import { listen } from "@tauri-apps/api/event";
+
+type SysEvent = {
+  time: number
+  name?: string
+}
+
+listen('event', (e) => {
+  console.log(e.payload)
+})
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  const [logs, setLogs] = useState<SysEvent[]>([])
+
+  useEffect(() => {
+    listen<SysEvent>('log', (e) => {
+      setLogs(s => [e.payload, ...s ])
+    })
+  },[])
+
+  return (
+    <div className="container">
+      <h1>Welcome to Tauri!</h1>
+
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <button onClick={() => invoke('test')}>
+        test
+      </button>
+
+      <div>
+        <button onClick={() => invoke('start_grab')}>Start grab</button>
+        <button onClick={() => invoke('stop_grab')}>Stop grab</button>
+      </div>
+
+      {logs.map(el => <div>{JSON.stringify(el)}</div>)}
+
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
+
+export default App;
